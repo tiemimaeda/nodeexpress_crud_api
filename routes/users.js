5189add23eb17642342f0c3dd522172b13f78ff8
tiemimaeda <priscila.maeda@yahoo.com.br>
2021-05-08 00:18:58 -0300

    removing comments

--- routes/users.js
+++ routes/users.js
@@ -3,20 +3,14 @@ import { createUser, getUsers, getUserById, deleteUser, updateUser } from '../co
 
 const router = express.Router();
 
-//get all users - GET request
 router.get('/', getUsers);
 
-//post new user - POST request
 router.post('/', createUser);
 
-//get a user by id - GET request
 router.get('/:id', getUserById);
 
-//delete a user by id - DELETE request
 router.delete('/:id', deleteUser);
 
-//update a user info - PATCH request chamges partially, 
-//while PUT request overrides all the info
 router.patch('/:id', updateUser);
 
 export default router;
